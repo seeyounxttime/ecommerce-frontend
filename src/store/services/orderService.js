0d673cbf395475d2e7223abc8ddf53cd57b672c6
Đặng Trương Minh Trí <87639905+seeyounxttime@users.noreply.github.com>
2023-12-06 15:35:46 +0700

    commit

--- src/store/services/orderService.js
+++ src/store/services/orderService.js
@@ -15,7 +15,9 @@ const orderService = createApi({
   endpoints: (builder) => {
     return {
       getOrders: builder.query({
+        // định nghĩa endpoints
         query: (page) => {
+          // lấy thông tin order
           return {
             url: `/orders?page=${page}`,
             method: "GET",
@@ -24,7 +26,9 @@ const orderService = createApi({
         providesTags: ["orders"],
       }),
       details: builder.query({
+        // định nghĩa endpoints
         query: (id) => {
+          // lấy chi tiết order
           return {
             url: `/order-details/${id}`,
             method: "GET",
@@ -33,7 +37,9 @@ const orderService = createApi({
         providesTags: ["orders"],
       }),
       deliverOrder: builder.mutation({
+        // định nghĩa endpoints
         query: (id) => {
+          // tình trạng order
           return {
             url: `/order-update?id=${id}&status=delivered`,
             method: "PUT",
